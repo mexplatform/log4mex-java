cfe6debcebb08584b3347b12bead56a66601eb28
diegoesteves <diegoesteves@gmail.com>
2017-11-19 16:28:03 +0100

    wasota

---
+++ src/main/java/org/metarchive/mex/log4mex/perf/IMeasure.java
@@ -0,0 +1,11 @@
+package org.metarchive.mex.log4mex.perf;
+
+/**
+ * Created by dnes on 08/05/16.
+ */
+public interface IMeasure {
+
+ //   public ArrayList<? extends Measure> getPerformances();
+
+
+}
